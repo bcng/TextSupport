ee8611f335ea480443d2a900da7d688603c2c251
Brian Ng <bcn220@gmail.com>
2015-08-10 13:59:44 -0600

    set up twilio post endpoint

--- server.js
+++ server.js
@@ -5,6 +5,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var request = require('request');
+var twilio = require('twilio');
+
+// Twilio Credentials 
+var accountSid = '';
+var authToken = '';
+
+// Create a REST client 
+var client = twilio(accountSid, authToken);
 
 var app = express();
 var port = 8899;
@@ -15,7 +24,7 @@ var port = 8899;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -24,14 +33,34 @@ app.use(cors());
 //  ENDPOINTS
 /////////////////////////////////////
 
-app.get('/api/message', function(req, res){
-	return res.send({"message": "Hello world!"});
+app.get('/api/message', function(req, res) {
+    res.send({
+        "message": "Hello world!"
+    });
 });
 
-app.post('/api/send_text_message', function(req, res){
-	res.send('Good');
+app.post('/api/send_text_message', function(req, res) {
+    console.log(req.body.message);
+
+    // request.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-04-01/Accounts/' + accountSid + '/Messages.json');
+
+    client.messages.create({
+        to: "917 903 0909",
+        from: "+19177465893",
+        body: "hey",
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+
 });
+
+
+
+
+
+
+
 
 app.listen(port, function() {
-	console.log("Listening on port " + port);
+    console.log("Listening on port " + port);
 })
