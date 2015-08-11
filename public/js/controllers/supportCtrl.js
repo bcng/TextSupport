d5936cf509591a28a48b85f3a7bd5d68b51da02c
Brian Ng <bcn220@gmail.com>
2015-08-11 14:52:46 -0600

    add public folder and display msg data on support html

--- public/js/controllers/supportCtrl.js
+++ public/js/controllers/supportCtrl.js
@@ -2,14 +2,8 @@ app.controller('supportCtrl', function($scope, $firebaseObject) {
 
 	// creates a new instance of FB db data
 	var firebaseRef = new Firebase('https://text-support-nodejs.firebaseio.com/numbers');
-
-	var obj = $firebaseObj(ref);
-
-	obj.$loaded().then(function(){
-		$scope.numbers = 
-	})
     
     // download the data into a local object
-    // $scope.numbers = $firebaseObject(firebaseRef);
+    $scope.numbers = $firebaseObject(firebaseRef);
 
 });
