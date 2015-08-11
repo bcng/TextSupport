8ccafd0b0d16f89d7b3d744422aa983988440e8e
Brian Ng <bcn220@gmail.com>
2015-08-11 09:02:39 -0600

    display conversation data on support page from firebase db

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var request = require('request');
 var twilio = require('twilio');
+var Firebase = require('firebase');
 
 // Twilio Credentials 
-var accountSid = '';
-var authToken = '';
+var accountSid = 'AC7d5471f4e18bccf9785561844d8c9467';
+var authToken = '31f2f18d45a7558640c645c410ccd735';
 
 // Create a REST client 
 var client = twilio(accountSid, authToken);
+
+// Create new Firebase ref
+var firebaseRef = new Firebase('https://text-support-nodejs.firebaseio.com/numbers');
 
 var app = express();
 var port = 8899;
@@ -21,8 +25,7 @@ var port = 8899;
 /////////////////////////////////////
 //  MIDDLEWARE
 /////////////////////////////////////
-
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -45,20 +48,14 @@ app.post('/api/send_text_message', function(req, res) {
     // request.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-04-01/Accounts/' + accountSid + '/Messages.json');
 
     client.messages.create({
-        to: "917 903 0909",
-        from: "+19177465893",
+        to: "9179030909",
+        from: "19177465893",
         body: "hey",
     }, function(err, message) {
         console.log(message.sid);
     });
 
 });
-
-
-
-
-
-
 
 
 app.listen(port, function() {
